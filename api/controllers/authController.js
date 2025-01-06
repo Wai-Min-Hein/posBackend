7a99d5347ef45db0756cad2cd48e103c3b869d97
Wai-Min-Hein <74950902+Wai-Min-Hein@users.noreply.github.com>
2025-01-06 16:06:06 +0630

    remove path

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -68,8 +68,6 @@ export const signIn = async (req, res, next) => {
         httpOnly: true,
         secure: process.env.NODE_ENV?.includes("production"), // Use secure only in production
         sameSite: "None", // Allow cross-origin usage
-        domain: ".vercel.app",
-        path: "/",
       })
       .json({ message: "User sign in successfully", user: rest,NODE_ENV: process.env.NODE_ENV,
         secure: process.env.NODE_ENV?.includes("production"), });
